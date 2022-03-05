7650d067813fa23112351764d68d895543cb2aca
Yusif Nazarbayov (CodeAcademy) <99797435+yusifny@users.noreply.github.com>
2022-03-05 11:10:48 +0400

    Add files via upload

---
+++ divisableTo3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace divisableTo3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6 };
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 3 == 0)
+                    sum += i;
+
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
