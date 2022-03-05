4753235fa1f0ee31f9c97e0b05553d1fe750711c
Yusif Nazarbayov (CodeAcademy) <99797435+yusifny@users.noreply.github.com>
2022-03-05 11:07:34 +0400

    file created
    file created

---
+++ digitNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace digitNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int count = 0;
+            while (number > 0)
+
+            {
+                number = number / 10;
+                count += 1;
+
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
